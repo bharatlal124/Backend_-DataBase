6177bfb3e87470a9734d830d34de75892ce36f29
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-02-16 22:23:43 +0530

    Add files via upload

---
+++ Node JS Revision/Adding Reviews for Authors and Books/src/features/books/review.schema.js
@@ -0,0 +1,25 @@
+// make necessary imports here
+import mongoose from "mongoose";
+// write your code here
+export const reviewSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  target: {
+    type: String,
+    enum: ["Author", "Book"],
+    required: true,
+  },
+  targetId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    refPath: "target",
+  },
+});
